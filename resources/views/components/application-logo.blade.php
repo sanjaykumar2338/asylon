49ea0c3bdabb476b79bc468bc62f34a423899048
Sanjay Kumar <sanjaykumar2338@yahoo.com>
2025-11-04 18:13:28 +0530

    new logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -2,4 +2,14 @@
     'alt' => config('app.name', 'Application'),
 ])
 
-<img src="{{ asset('assets/images/new-logo.png') }}" alt="{{ $alt }}" {{ $attributes->merge(['class' => 'h-10 w-auto']) }}>
+@php
+    $existingStyle = trim((string) $attributes->get('style'));
+    $style = 'background-color: currentColor;' . ($existingStyle !== '' ? ' ' . $existingStyle : '');
+@endphp
+
+<img
+    src="{{ asset('assets/images/new-logo.png') }}"
+    alt="{{ $alt }}"
+    {{ $attributes->merge(['class' => 'h-10 w-auto'])->except('style') }}
+    style="{{ $style }}"
+>
